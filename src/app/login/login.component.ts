bfaf1a38593cd4bbf30b430ed5222a057b56c9e3
johnkalog <JohnKal97@gmail.com>
2020-12-03 02:54:25 +0200

    login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  private formGroup: FormGroup;
+  private show = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.formGroup = new FormGroup({
+      login: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+    });
   }
 
+  public get btnDisabled(): boolean {
+    return this.formGroup.invalid;
+  }
+
+  public onBtnClick(): void {
+    alert("ety");
+  }
+
+  public showTheHint(): void {
+    this.show = !this.show;
+  }
+
+  public get showOrHide(): string {
+    return `${this.show ? "Hide" : "Show"} hint`;
+  }
 }
